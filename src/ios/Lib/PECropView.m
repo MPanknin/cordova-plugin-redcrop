3aaf96714930606ef8a1d002cb793df2f01cafd0
Martin Panknin <m@redplant.de>
2016-09-12 19:07:31 +0200

    Debug scroll view params

--- src/ios/Lib/PECropView.m
+++ src/ios/Lib/PECropView.m
@@ -457,6 +457,20 @@ static const CGFloat MarginLeft = 20.0f;
     if ([self.msgDelegate respondsToSelector:@selector(msg2Client:)]) {
         [self.msgDelegate msg2Client:@"redLog('Hi from the UI... Crop End');"];
     }
+
+    CGSize size = self.scrollView.contentSize;
+
+    if ([self.msgDelegate respondsToSelector:@selector(msg2Client:)]) {
+        NSString *msg = [NSString stringWithFormat:@"redLog('Content size:  %f %f');", size.width , size.height];
+        [self.msgDelegate msg2Client:msg];
+    }    
+
+    CGFloat zoomScale = self.scrollView.zoomScale;
+
+    if ([self.msgDelegate respondsToSelector:@selector(msg2Client:)]) {
+        NSString *msgScale = [NSString stringWithFormat:@"redLog('Zoom scale:  %f');", zoomScale];
+        [self.msgDelegate msg2Client:msgScale];
+    }      
 }
 
 - (void)zoomToCropRect:(CGRect)toRect andCenter:(BOOL)center
